c138d6624c9e0d11e4642e5837c4c8b5022866e6
Gascor <lucas-dasilvaferreira@hotmail.com>
2024-09-20 15:42:50 +0200

    first commit;

---
+++ UneFenetre.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+import javax.swing.*;
+
+class UneFenetre extends JFrame 
+{
+    UnMobile sonMobile;
+    private final int LARG=400, HAUT=250;
+    
+    public UneFenetre()
+    {
+	sonMobile = new UnMobile(LARG, HAUT);
+	Thread laTache = new Thread(sonMobile);
+    this.add(sonMobile);
+    this.setVisible(true);
+	laTache.start();
+
+    }
+}
