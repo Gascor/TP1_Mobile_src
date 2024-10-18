a0265a141ac014f1e212491b24d8a059222aebcb
Gascor <lucas-dasilvaferreira@hotmail.com>
2024-10-18 02:31:47 +0200

    feat (Main.java): Ajout de la classe Client "Main" Test validée Succession de dépot et reprise des lettre respectivement A B C D et E (Rattrapage du cours du 11/10/2024);

---
+++ TP3_BAL_src/Main.java
@@ -0,0 +1,12 @@
+package TP3_BAL_src;
+
+public class Main {
+    public static void main(String[] args) {
+        BAL bal = new BAL("", true);  // La boîte est vide au début
+        Producteur prod = new Producteur("ABCDE", bal);  // Producteur de lettres
+        Consommateur cons = new Consommateur(bal);  // Consommateur
+
+        prod.start();  // Lancer le producteur
+        cons.start();  // Lancer le consommateur
+    }
+}
