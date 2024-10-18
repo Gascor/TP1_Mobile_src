9d4b139edbcccc483df43a398817bdbd10fab9fe
Gascor <lucas-dasilvaferreira@hotmail.com>
2024-10-18 02:29:43 +0200

    feat (Producteur.java): Ajout de la classe "Producteur" (Rattrapage du cours du 11/10/2024);

---
+++ TP3_BAL_src/Producteur.java
@@ -0,0 +1,24 @@
+package TP3_BAL_src;
+
+public class Producteur extends Thread {
+    String lettre;
+    BAL chbal;
+
+    public Producteur(String parlettre, BAL parbal) {
+        lettre = parlettre;
+        this.chbal = parbal;
+    }
+
+    public void run() {
+        try {
+            for (int i = 0; i < lettre.length(); i++) {
+                // Dépose chaque lettre dans la boîte
+                chbal.write(String.valueOf(lettre.charAt(i)));
+                System.out.println("Lettre déposée : " + lettre.charAt(i));
+                Thread.sleep(1000);  // Simule un délai dans la production
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
