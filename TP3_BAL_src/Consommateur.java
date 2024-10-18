0b88aec0a3f3dfb62e2c02108b457c4206a2460a
Gascor <lucas-dasilvaferreira@hotmail.com>
2024-10-18 02:44:26 +0200

    fix (Main.java): Simplification de code (Rattrapage du cours du 11/10/2024);

--- TP3_BAL_src/Consommateur.java
+++ TP3_BAL_src/Consommateur.java
@@ -1,17 +1,21 @@
 package TP3_BAL_src;
 
-public class Consommateur {
-    private BAL chbal;
+public class Consommateur extends Thread  {
+    BAL chbal;
 
     public Consommateur(BAL parbal) {
         this.chbal = parbal;
     }
 
-    public void retirerLettre() throws InterruptedException {
-        while (true) {
-            String lettre = chbal.read();
-            System.out.println("Lettre retirée : " + lettre);
-            Thread.sleep(1500);
+    public void run() {
+        try {
+            while (true) {
+                String lettre = chbal.read();
+                System.out.println("Lettre retirée : " + lettre);
+                Thread.sleep(1500);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
--- TP3_BAL_src/Producteur.java
+++ TP3_BAL_src/Producteur.java
@@ -1,19 +1,23 @@
 package TP3_BAL_src;
 
-public class Producteur {
-    private String lettre;
-    private BAL chbal;
+public class Producteur extends Thread {
+    String lettre;
+    BAL chbal;
 
     public Producteur(String parlettre, BAL parbal) {
-        this.lettre = parlettre;
+        lettre = parlettre;
         this.chbal = parbal;
     }
 
-    public void deposerLettre() throws InterruptedException {
-        for (int i = 0; i < lettre.length(); i++) {
-            chbal.write(String.valueOf(lettre.charAt(i)));
-            System.out.println("Lettre déposée : " + lettre.charAt(i));
-            Thread.sleep(1000);
+    public void run() {
+        try {
+            for (int i = 0; i < lettre.length(); i++) {
+                chbal.write(String.valueOf(lettre.charAt(i)));
+                System.out.println("Lettre déposée : " + lettre.charAt(i));
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
